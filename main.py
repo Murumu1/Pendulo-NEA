d6f49a0b2163e34ec85662eeaf9752a20ac6efb2
Murtaza Daudali <murumu1@yahoo.com>
2021-01-06 15:51:46 +0000

    Container system

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from collections import namedtuple
 
 import pygame
-
+from pygame import Rect
+from pygame.font import Font
+from random import randint
+from typing import Union
 import sys
 import math
 from math import sin, pi, sqrt
-from dataclasses import dataclass
+from dataclasses import dataclass, astuple
 import colorsys
 
 # Initialises the pygame module
@@ -28,10 +31,16 @@ paused = False
 speed = 1
 
 
-def text(screen, font, text, colour, x, y):
+@dataclass()
+class Text:
+    text_obj: Font
+    text_rect: Rect
+
+
+def text(font, text, colour, center):
     t = font.render(text, True, colour)
-    t_r = t.get_rect(center=(x, y))
-    screen.blit(t, t_r)
+    t_r = t.get_rect(center=center)
+    return Text(t, t_r)
 
 
 def darken(colour: tuple, percent=0):
@@ -51,7 +60,12 @@ class Pendulum:
 
     # Returns x, y relative to time (t)
     def get_value(self, t) -> float:
-        return self.amplitude * 100 * sin(t * self.frequency + self.phase) * pow(math.e, -self.damping * t)
+        return (
+            self.amplitude
+            * 100
+            * sin(t * self.frequency + self.phase)
+            * pow(math.e, -self.damping * t)
+        )
 
 
 @dataclass
@@ -61,134 +75,104 @@ class SliderInfo:
     default: float
 
 
-# Slider class to manage changing values (i is the index of the Slider)
 SLIDER_MOVED = pygame.USEREVENT + 1
-slider_event = pygame.event.Event(SLIDER_MOVED, name="SLIDER_MOVED")
+CONTAINER_HOVER = pygame.USEREVENT + 2
 
 
-class Widget:
-    def __init__(self, tag, size):
-        self.tag = tag
+class Container:
+    def __init__(
+        self, tag: str, container_size: tuple[int, int], center: tuple[int, int]
+    ):
+        self.container_rect = Rect((0, 0), container_size)
+        self.container_rect.center = center
+        self.container_event = pygame.event.Event(CONTAINER_HOVER, name=tag)
         self.size = size
 
+    def is_hover(self):
+        if self.container_rect.collidepoint(pygame.mouse.get_pos()):
+            return True
+        else:
+            return False
 
-class Slider:
-    def __init__(self, name, min_val, max_val, i, default):
+
+class Widget(Container):
+    def __init__(self, tag, rects: list[Rect], texts: list[Text], function):
+        self.function = function
+        self.widget_rect = rects[0].unionall(rects + [text.text_rect for text in texts])
+        self.rects = rects
+        self.texts = texts
+
+        super(Widget, self).__init__(
+            tag, self.widget_rect.size, self.widget_rect.center
+        )
+
+    def on_click(self):
+        if self.is_hover() and pygame.mouse.get_pressed()[0]:
+            self.function()
+
+    def draw(self, colour=white):
+        for rect in self.rects:
+            pygame.draw.rect(screen, colour, rect)
+        for text in self.texts:
+            screen.blit(text.text_obj, text.text_rect)
+
+
+class Slider(Widget):
+    def __init__(self, tag, min_val, max_val, index, default, function=None):
         self.min_val = min_val
         self.max_val = max_val
-        self.i = i
-        self.name = name
+        self.i = index
+        self.tag = tag
         self.default = default
 
         # x positions
-        self.bar_start = 50
-        self.bar_end = 250
-        self.bar_length = self.bar_start + self.bar_end
+        # self.bar_start = 50
+        # self.bar_end = 250
+        # self.bar_length = self.bar_start + self.bar_end
 
-        # Rect object which is the slider-ball
-        self.r = pygame.Rect((0, 0), (30, 30))
+        self.r = Rect((0, 0), (30, 30))
         self.r.center = (
-            ((self.default * self.bar_length) / (self.max_val - self.min_val)) + 50,
+            ((self.default * 300) / (self.max_val - self.min_val)) + 50,
             h - (self.i * 70) + 30,
         )
 
-        self.container = pygame.Rect((0, 0), (self.bar_length, 70))
-        self.container.center = (self.bar_length / 2, self.r.centery)
+        self.line_rect = Rect((50, self.r.centery), (201, 1))
+        self.rects = [self.r, self.line_rect]
 
+        self.font = pygame.font.SysFont("Arial", 15)
+        self.min_val_label = text(
+            self.font, str(self.min_val), white, (20, self.r.centery)
+        )
+        self.max_val_label = text(
+            self.font, str(self.max_val), white, (280, self.r.centery)
+        )
+        self.tag_label = text(
+            self.font, self.tag, white, (self.line_rect.centerx, self.r.centery - 30)
+        )
+        self.texts = [self.min_val_label, self.max_val_label, self.tag_label]
+
+        super(Slider, self).__init__(tag, self.rects, self.texts, function)
+
+        self.slider_event = pygame.event.Event(SLIDER_MOVED, tag=tag, slider_id=index)
         self.value = self.default
         self.colour = white
-        self.font = pygame.font.SysFont("Arial", 15)
-
-    def draw(self):
-
-        # Draws the line for the slider-ball to follow
-        l = pygame.draw.line(
-            screen,
-            white,
-            (self.bar_start, self.r.centery),
-            (self.bar_end, self.r.centery),
-        )
-
-        # Name of the slider
-        text(screen, self.font, self.name, white, l.center[0], self.r.centery - 30)
-
-        # Minimum value text
-        text(
-            screen,
-            self.font,
-            str(self.min_val),
-            white,
-            self.bar_start - 30,
-            self.r.centery,
-        )
-
-        # Maximum value text
-        text(
-            screen,
-            self.font,
-            str(self.max_val),
-            white,
-            self.bar_end + 30,
-            self.r.centery,
-        )
-
-        # Triggers if the mouse is hovering over the slider-ball
-        if (
-            self.r.left < pygame.mouse.get_pos()[0] < self.r.left + 30
-            and self.r.top < pygame.mouse.get_pos()[1] < self.r.top + 30
-        ):
-
-            pygame.draw.rect(screen, darken(self.colour), self.r)
-
-            if pygame.mouse.get_pressed()[0]:
-                self.move_rect(pygame.mouse.get_pos()[0])
-
-        else:
-
-            pygame.draw.rect(screen, self.colour, self.r)
 
     # Moves the slider-ball to chosen position
-
     def move_rect(self, new_x):
 
-        if self.bar_end > new_x > self.bar_start:
+        if 250 > new_x > 50:
             self.r.center = (new_x, self.r.centery)
 
-            self.value = (
-                (self.max_val - self.min_val) / self.bar_length
-            ) * self.r.centerx
+            self.value = ((self.max_val - self.min_val) / 300) * self.r.centerx
 
         return self.r.center
 
 
-class Button:
-    def __init__(self, rect, func):
-        self.rect = pygame.Rect(rect)
-        self.func = func
-
-    def draw(self, col):
-        if (
-            self.rect.left
-            < pygame.mouse.get_pos()[0]
-            < self.rect.left + self.rect.width
-            and self.rect.top
-            < pygame.mouse.get_pos()[1]
-            < self.rect.top + self.rect.height
-        ):
-            pygame.draw.rect(screen, darken(col), self.rect)
-        else:
-            pygame.draw.rect(screen, col, self.rect)
-
-    def call(self):
-        if (
-            self.rect.left
-            < pygame.mouse.get_pos()[0]
-            < self.rect.left + self.rect.width
-            and self.rect.top
-            < pygame.mouse.get_pos()[1]
-            < self.rect.top + self.rect.height
-        ):
-            self.func()
+class Button(Widget):
+    def __init__(self, tag, rect: Rect, function):
+        self.font = pygame.font.SysFont("Arial", 15)
+        self.tag_label = text(self.font, tag, black, rect.center)
+        super(Button, self).__init__(tag, [rect], [self.tag_label], function)
 
 
 slider_info = {
@@ -214,9 +198,9 @@ for i in range(0, 8):
     sliders.append(Slider(key, value.min_val, value.max_val, i + 1, value.default))
 
 
-container_size = sum(slider.container.height for slider in sliders) / 2, sum(slider.container.width for slider in sliders) / 2
-container_rect = pygame.Rect(sliders[len(sliders)-1].container.topleft, container_size)
-pygame.draw.rect(screen, (255, 255, 255), container_rect)
+# container_size = sum(slider.container.height for slider in sliders) / 2, sum(slider.container.width for slider in sliders) / 2
+# container_rect = Rect(sliders[len(sliders)-1].container.topleft, container_size)
+# pygame.draw.rect(screen, (255, 255, 255), container_rect)
 
 
 # Converts cartesian coordinates to pygame
@@ -247,24 +231,25 @@ def inc_speed():
         speed *= 2
 
 
-quit_btn = Button((10, 10, 60, 60), pygame.quit)
-pause = Button((80, 10, 60, 60), pause)
-cls = Button((150, 10, 60, 60), fill_black)
-spd = Button((220, 10, 60, 60), inc_speed)
+quit_btn = Button("Quit", Rect(10, 10, 60, 60), pygame.quit)
+pause = Button("Pause", Rect(80, 10, 60, 60), pause)
+cls = Button("Clear", Rect(150, 10, 60, 60), fill_black)
+spd = Button("Speed", Rect(220, 10, 60, 60), inc_speed)
 
 last_point = ()
-while 1:
+running = True
+while running:
     screen.fill((0, 0, 0), (0, 70, 300, h))
 
     # Event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
+            running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            quit_btn.call()
-            pause.call()
-            cls.call()
-            spd.call()
+            quit_btn.on_click()
+            pause.on_click()
+            cls.on_click()
+            spd.on_click()
 
     quit_btn.draw((255, 0, 0))
     x = Pendulum(sliders[0].value, sliders[1].value, sliders[2].value, sliders[3].value)
@@ -273,16 +258,11 @@ while 1:
     for slider in sliders:
         slider.draw()
         # Triggers if the mouse is hovering over the slider-ball
-
-        if (
-            slider.r.left < pygame.mouse.get_pos()[0] < slider.r.left + 30
-            and slider.r.top < pygame.mouse.get_pos()[1] < slider.r.top + 30
-        ):
-
+        if slider.is_hover():
             slider.colour = darken(slider.colour)
             if pygame.mouse.get_pressed()[0]:
                 slider.move_rect(pygame.mouse.get_pos()[0])
-                pygame.event.post(slider_event)
+                pygame.event.post(slider.slider_event)
 
         else:
             slider.colour = white
@@ -291,13 +271,21 @@ while 1:
 
         # Draws each point of the Harmonograph
         if SLIDER_MOVED in (ev.type for ev in pygame.event.get()):
-            x = Pendulum(sliders[0].value, sliders[1].value, sliders[2].value, sliders[3].value)
-            y = Pendulum(sliders[4].value, sliders[5].value, sliders[6].value, sliders[7].value)
+            x = Pendulum(
+                sliders[0].value, sliders[1].value, sliders[2].value, sliders[3].value
+            )
+            y = Pendulum(
+                sliders[4].value, sliders[5].value, sliders[6].value, sliders[7].value
+            )
 
         curr_values = curr_x, curr_y = x.get_value(time), y.get_value(time)
         if last_point:
-            pygame.draw.line(screen, (0, 255, 0), tp(last_point, size), tp(curr_values, size), 2)
-        next_values = next_x, next_y = x.get_value(time + speed / fps), y.get_value(time + speed / fps)
+            pygame.draw.line(
+                screen, (0, 255, 0), tp(last_point, size), tp(curr_values, size), 2
+            )
+        next_values = next_x, next_y = x.get_value(time + speed / fps), y.get_value(
+            time + speed / fps
+        )
         last_point = next_values
 
         point_distance = sqrt(((curr_x - next_x) ** 2) + ((curr_y - next_y) ** 2))
@@ -307,14 +295,18 @@ while 1:
 
         while point_distance > 1:
             next_stamp = speed / fps / (2 ** step)
-            general_point = x.get_value(time + next_stamp), y.get_value(time + next_stamp)
+            general_point = x.get_value(time + next_stamp), y.get_value(
+                time + next_stamp
+            )
 
             for s in range(1, 2 ** step, 2):
                 varying_point = time + s * next_stamp
                 vertex = x.get_value(varying_point), y.get_value(varying_point)
                 points.insert(1, vertex)
 
-            point_distance = sqrt(((curr_x - general_point[0]) ** 2) + ((curr_y - general_point[1]) ** 2))
+            point_distance = sqrt(
+                ((curr_x - general_point[0]) ** 2) + ((curr_y - general_point[1]) ** 2)
+            )
             step += 1
 
         def round_points(point):
@@ -335,3 +327,5 @@ while 1:
     clock.tick(fps)
 
     pygame.display.flip()  # Updates the display
+
+pygame.quit()
